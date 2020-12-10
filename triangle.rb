b8908b883c9a8f1bb7117d168f107249c7eae4ba
Anton <zxzsparrow@gmail.com>
2020-12-10 11:12:47 +0200

    completed koans

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,15 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  fail TriangleError, "Illegal triangle: [#{a}, #{b}, #{c}]" if
+    [a, b, c].max >= (a + b + c) / 2.0
+
+  return :equilateral if a == b and b == c
+  return :isosceles if a == b or b == c or a == c
+  return :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
+
 end
